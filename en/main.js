daf5430dff2d37cba42c3df5d397473b011d0cdf
Sam <gudwbot@gmail.com>
2024-11-18 15:45:37 +0330

    t6

---
+++ en/main.js
@@ -0,0 +1,10 @@
+import './en js/enCasualMainPage.js';
+// import './en js/enSideMenu.js';
+import '../mainJs/slider.js';
+import './en js/enJSAnimation.js';
+import '../mainJs/general.js';
+
+
+
+
+
