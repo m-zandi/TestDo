05cd21e27f6c861da1d2aefc50bed9a4a44f12ff
Sam <gudwbot@gmail.com>
2024-11-19 18:12:05 +0330

    tt2

---
+++ en/main.js
@@ -0,0 +1,10 @@
+import './en_js/enCasualMainPage.js';
+// import './en js/enSideMenu.js';
+import '../mainJs/slider.js';
+import './en_js/enJSAnimation.js';
+import '../mainJs/general.js';
+
+
+
+
+
